5d33a91dd4ce7ce9d8bf6a79108af3d6b59d9c02
LittleChapa <Daaavid943@gmail.com>
2023-12-20 01:27:04 +0300

    Сделана секция 'Отправить заявку'

--- scripts/dropDownList.js
+++ scripts/dropDownList.js
@@ -1,27 +1,62 @@
 // Каталог автомобилей
-const button = document.querySelector('.catalog__list');
-const listSelected = document.querySelector('.catalog__list-selected');
-const dropDownlist = document.querySelector('.catalog__list-select');
-const listItems = document.querySelectorAll('.catalog__list-item');
-const listArrow = document.querySelector('#dropdown-list-arrow');
+const catalogButton = document.querySelector('.catalog__list');
+const catalogListSelected = document.querySelector('.catalog__list-selected');
+const catalogDropDownList = document.querySelector('.catalog__list-select');
+const catalogListItems = document.querySelectorAll('.catalog__list-item');
+const catalogListArrow = document.querySelector('#catalog-dropdown-list-arrow');
 
-button.addEventListener('click', () => {
-  dropDownlist.classList.toggle('catalog__list-select_active');
-  listArrow.classList.toggle('catalog__list-arrow_active');
-});
-
-function dropDownList(arrayList, className) {
+function dropDownList(
+  button,
+  dropDownList,
+  arrow,
+  arrayList,
+  selected,
+  dropDownListClassName,
+  arrowClassName,
+  arrayListClassName
+) {
+  button.addEventListener('click', () => {
+    dropDownList.classList.toggle(dropDownListClassName);
+    arrow.classList.toggle(arrowClassName);
+  });
   arrayList.forEach((listItem) => {
     listItem.addEventListener('click', () => {
-      listItems.forEach((item) => {
-        item.classList.remove(className);
+      arrayList.forEach((item) => {
+        item.classList.remove(arrayListClassName);
       });
-      listItem.classList.add(className);
-      listSelected.innerText = listItem.innerText;
+      listItem.classList.add(arrayListClassName);
+      selected.innerText = listItem.innerText;
     });
   });
 }
-dropDownList(listItems, 'catalog__list-item_selected');
+dropDownList(
+  catalogButton,
+  catalogDropDownList,
+  catalogListArrow,
+  catalogListItems,
+  catalogListSelected,
+  'catalog__list-select_active',
+  'catalog__list-arrow_active',
+  'catalog__list-item_selected'
+);
+
+// Отправить заявку
+const feedbackButton = document.querySelector('.feedback__form-list');
+const feedbackListSelected = document.querySelector('.feedback__form-list-selected');
+const feedbackDropDownList = document.querySelector('.feedback__form-list-select');
+const feedbackListItems = document.querySelectorAll('.feedback__form-list-item');
+const feedbackListArrow = document.querySelector('#feedback-dropdown-list-arrow');
+
+dropDownList(
+  feedbackButton,
+  feedbackDropDownList,
+  feedbackListArrow,
+  feedbackListItems,
+  feedbackListSelected,
+  'feedback__form-list-select_active',
+  'feedback__form-list-arrow_active',
+  'feedback__form-list-item_selected'
+);
 
 // Часто задаваемые вопросы
 
