25b5e432e1fc987bcb58f420bdc6c494b4478fae
mokujin <mokujinn94@gmail.com>
2024-01-16 21:36:54 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const express = require('express');
-const sequelize = require('./db');
-const models = require('./models/models');
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
-const router = require('./routes/index');
-const errorHandler = require('./middleware/ErrorHandlingMiddleware');
-const path = require('path');
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const express = require("express");
+const sequelize = require("./db");
+const models = require("./models/models");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const router = require("./routes/index");
+const errorHandler = require("./middleware/ErrorHandlingMiddleware");
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 
@@ -16,15 +16,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, 'static')));
-app.use(express.static(path.resolve(__dirname, '../client')));
-app.use('/static', createProxyMiddleware({
-  target: 'http://localhost:3000',
-  changeOrigin: true,
-}));
+app.use(express.static(path.resolve(__dirname, "static")));
 app.use(fileUpload({}));
-('');
-app.use('/api', router);
+app.use("/api", router);
 
 app.use(errorHandler);
 
