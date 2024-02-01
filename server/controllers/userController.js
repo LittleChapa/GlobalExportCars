3ff9900e1b6a8688175614d317701fa509434e15
Проект 26Studio <26Studio@26kadr.internal>
2024-02-01 16:12:28 +0300

    registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -11,6 +11,25 @@ const generateJwt = (id, login, role) => {
 };
 
 class UserController {
+  async registration(req, res, next) {
+    const { login, password } = req.body;
+
+    if (!login || !password) {
+      return next(ApiError.badRequest("Некорректный email или password"));
+    }
+
+    const condidate = await User.findOne({ where: { login } });
+
+    if (condidate) {
+      return next(ApiError.badRequest("Ошибка"));
+    }
+
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ login, password: hashPassword });
+    const token = generateJwt(user.id, user.login, user.role);
+    return res.json({ token });
+  }
+
   async login(req, res, next) {
     const { login, password } = req.body;
     const user = await User.findOne({ where: { login } });
