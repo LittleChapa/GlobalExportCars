e3f15d0c3393d0e431ad75b6a2323061092275c5
Проект 26Studio <26Studio@26kadr.internal>
2024-02-01 16:06:06 +0300

    security

--- server/routes/applicationRouter.js
+++ server/routes/applicationRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express');
 const applicationController = require('../controllers/applicationController');
+const authMiddleware = require('../middleware/authMiddleware')
 const router = new Router();
 
-router.patch('/update/:id', applicationController.update);
+router.patch('/update/:id', authMiddleware, applicationController.update);
 router.get('/', applicationController.getAll);
 
 module.exports = router;
